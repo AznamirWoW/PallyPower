fde3509a878834cd2569fdb5bf9872bec79c230f
aznamir <aznamir@281a127e-c2b9-49b4-b9ef-5bb33d143235>
2010-05-15 04:28:21 +0000

    Tagging as b3.2.13

---
+++ tags/b3.2.13/locale/esES.lua
@@ -0,0 +1,5 @@
+local L = AceLibrary("AceLocale-2.2"):new("PallyPower")
+
+L:RegisterTranslations("esES", function() return 
+--@localization(locale="esES", format="lua_table", handle-unlocalized="english")@ 
+end)
