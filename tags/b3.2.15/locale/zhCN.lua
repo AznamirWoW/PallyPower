42f460e6d1a02003449fd3eabd6715e8b6d481e8
aznamir <aznamir@281a127e-c2b9-49b4-b9ef-5bb33d143235>
2010-06-25 04:29:30 +0000

    Tagging as b3.2.15

---
+++ tags/b3.2.15/locale/zhCN.lua
@@ -0,0 +1,5 @@
+﻿local L = AceLibrary("AceLocale-2.2"):new("PallyPower")
+
+L:RegisterTranslations("zhCN", function() return 
+--@localization(locale="zhCN", format="lua_table", handle-unlocalized="english")@ 
+end)
