833ee71cc09717b304cd18a1088ad7dc0758aaac
aznamir <aznamir@281a127e-c2b9-49b4-b9ef-5bb33d143235>
2010-07-16 04:56:44 +0000

    Tagging as b3.2.17

---
+++ tags/b3.2.17/locale/deDE.lua
@@ -0,0 +1,5 @@
+local L = AceLibrary("AceLocale-2.2"):new("PallyPower")
+
+L:RegisterTranslations("deDE", function() return
+--@localization(locale="deDE", format="lua_table", handle-unlocalized="english")@
+end)	
