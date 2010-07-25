26e67e930d70c737226fde00f874f9fff774e4d4
aznamir <aznamir@281a127e-c2b9-49b4-b9ef-5bb33d143235>
2010-07-25 17:35:54 +0000

    Tagging as v3.2.19

---
+++ tags/v3.2.19/locale/esMX.lua
@@ -0,0 +1,5 @@
+local L = AceLibrary("AceLocale-2.2"):new("PallyPower")
+
+L:RegisterTranslations("esMX", function() return 
+--@localization(locale="esMX", format="lua_table", handle-unlocalized="english")@ 
+end)
