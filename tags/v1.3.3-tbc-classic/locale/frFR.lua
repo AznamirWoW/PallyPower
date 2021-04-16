527a0de7db1b4c49f4baeb187002e79ccc663c76
EsWoW <EsWoW@281a127e-c2b9-49b4-b9ef-5bb33d143235>
2021-04-16 19:11:39 +0000

    Created tag v1.3.3-tbc-classic.

---
+++ tags/v1.3.3-tbc-classic/locale/frFR.lua
@@ -0,0 +1,3 @@
+﻿local L = LibStub("AceLocale-3.0"):NewLocale("PallyPower", "frFR", false, false)
+if not L then return end 
+--@localization(locale="frFR", format="lua_additive_table", handle-unlocalized="english")@ 
