9af3c8b5a271ab0cc5a3942092ec7d61f861b3a9
gallantron <gallantron@281a127e-c2b9-49b4-b9ef-5bb33d143235>
2022-03-25 17:17:33 +0100

    allow pallypower to load from a repository clone in enUS

--- locale/enUS.lua
+++ locale/enUS.lua
@@ -1,3 +1,9 @@
-local L = LibStub("AceLocale-3.0"):NewLocale("PallyPower", "enUS", true, false)
+local silent = false
+--@debug@
+silent = true
+--@end-debug@
+
+local L = LibStub("AceLocale-3.0"):NewLocale("PallyPower", "enUS", true, silent)
 if not L then return end
+
 --@localization(locale="enUS", format="lua_additive_table", handle-unlocalized="english")@ 
